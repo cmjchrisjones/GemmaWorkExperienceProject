8a41e4e940c27017fd5d426f4f735fee57df453c
Chris Jones <cmj.chris.jones@googlemail.com>
2019-09-08 20:49:57 +0100

    Initial commit of Gemma's Work Experience project

---
+++ Pages/RequestCake.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CakeSale_GemmaWorkExperience.Pages
+{
+    public class RequestCakeModel : PageModel
+    {
+        [BindProperty]
+        [Required(ErrorMessage = "Your customer errror message")]
+        public string customername { get; set; }
+        [BindProperty]
+        public string caketype { get; set; }
+        public void OnGet()
+        {
+
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid) { return Page(); }
+            return Page();
+
+        }
+    }
+}
